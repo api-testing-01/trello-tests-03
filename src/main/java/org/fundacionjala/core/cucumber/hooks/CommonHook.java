a88915d2476f91629fba9c42408134f3cc619a23
jhasmany_quiroz <jhasmany.quiroz@jalasoft.com>
2019-11-26 03:49:03 -0400

    Added a before hook to create base objects

--- src/main/java/org/fundacionjala/core/cucumber/hooks/CommonHook.java
+++ src/main/java/org/fundacionjala/core/cucumber/hooks/CommonHook.java
@@ -3,8 +3,10 @@ package org.fundacionjala.core.cucumber.hooks;
 import java.util.List;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.restassured.specification.RequestSpecification;
 
+import org.fundacionjala.core.api.DynamicIdHelper;
 import org.fundacionjala.core.api.RequestManager;
 import org.fundacionjala.core.ScenarioContext;
 
@@ -16,6 +18,17 @@ public class CommonHook {
         this.context = context;
     }
 
+    @Before(value = "@createBaseObjects")
+    public void beforeEscenario() {
+        String jsonBody = "{\"name\": \"Board1 created by cucumber\"}";
+        String endpoint = "/boards";
+        createObject(endpoint, jsonBody);
+
+        endpoint = "/lists";
+        jsonBody = "{\"name\": \"To_Do\",\"idBoard\": \"(B.id)}";
+        createObject(endpoint, jsonBody);
+    }
+
     @After(value = "@cleanData")
     public void afterScenario() {
         RequestSpecification requestSpec = (RequestSpecification) context.get("REQUEST_SPEC");
@@ -23,5 +36,12 @@ public class CommonHook {
         for (String endpoint : endpoints) {
             RequestManager.delete(requestSpec, endpoint);
         }
+    }
+
+    public void createObject(String endPoint, String jsonBody) {
+        RequestSpecification requestSpecification = (RequestSpecification) context.get("REQUEST_SPEC");
+        String builtEndpoint = DynamicIdHelper.buildEndpoint(context, endPoint);
+        RequestManager.doRequest("POST", requestSpecification, builtEndpoint,
+                DynamicIdHelper.replaceIds(context, jsonBody));
     }
 }
